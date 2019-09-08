9893a12c23bc248ab04657fe5a574f0c9f5d907d
KoinoEngineering <email@koino.engineering>
2019-09-08 17:53:45 +0900

    #9 1文字目の0を削除

--- src/common/NumberBox/state.ts
+++ src/common/NumberBox/state.ts
@@ -11,6 +11,7 @@ export const initialNumberBoxState = (state?: Partial<INumberBoxState>): INumber
   ...state
 })
 
-export const changeNumberBox = (value: any) => (state: IImNumberBoxState): IImNumberBoxState => {
-  return isNaN(Number(value)) ? state : state.set("value", value);
+export const changeNumberBox = (value: string) => (state: IImNumberBoxState): IImNumberBoxState => {
+  const stdValue = (value !== "0" && value.charAt(0) === "0") ? value.substr(1) : value;
+  return isNaN(Number(stdValue)) ? state : state.set("value", stdValue);
 }
