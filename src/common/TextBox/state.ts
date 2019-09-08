db8c9067192c8d1c0ef6d38beb94a81917b5afcb
KoinoEngineering <email@koino.engineering>
2019-09-08 17:27:42 +0900

    #9 テキストボックスの型を数字に

--- src/common/TextBox/state.ts
+++ src/common/TextBox/state.ts
@@ -1,12 +1,12 @@
 import { IStateBase } from "../../core/State";
 
 export interface ITextBoxState {
-  value: string
+  value: number
 }
 
 export type IImTextBoxState = IStateBase<ITextBoxState>
 
 export const initialTextBoxState = (state?: Partial<ITextBoxState>): ITextBoxState => ({
-  value: "",
+  value: 0,
   ...state
 })
